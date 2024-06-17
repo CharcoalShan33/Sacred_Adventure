823f4712b936d77f7c6fc1c88e82e39105c7e275
CharcoalShan33 <128164780+CharcoalShan33@users.noreply.github.com>
2024-06-17 02:30:37 -0400

    Added new files

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,28 +12,73 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float playerSpeed;
 
+    [SerializeField]
+    private float jumpVelocity;
+
+    private bool isGrounded;
+
+    [SerializeField]
+    float groundCheckDist;
+
+    [SerializeField]
+    LayerMask layer;
+
+    private Animator anim;
+
+    public bool isWalking = true;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+     
+        HandleCollision();
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+        }
+        HandleAnimation();
+         HandleMovement();
         
     }
 
-    private void FixedUpdate()
+    
+
+    private void HandleJump()
     {
-        xInput = Input.GetAxis("Horizontal");
-        yInput = Input.GetAxis("Vertical");
-        HandleMovement();
+        
+
+        
+    }
+
+    private void HandleAnimation()
+    {
+       
+        anim.SetBool("isWalking", isWalking);
+        //anim.SetFloat("xMove", rb.velocity.x);
+    }
+    private void HandleCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDist, layer);
+
     }
 
     private void HandleMovement()
     {
         rb.velocity = new Vector3(xInput, yInput, 0f) * playerSpeed;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDist, 0f));
     }
 }
